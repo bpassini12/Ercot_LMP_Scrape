bfdd73923a2d6320b31be4804f90896376c8f49b
Brett Passini <Brett.Passini@Gmail.com>
2022-02-19 19:54:44 -0600

    add bp_sql, delete old sqlite funcs, stop using openpyxl

--- SPP_Download.py
+++ SPP_Download.py
@@ -5,28 +5,31 @@ import pandas as pd
 import re
 import pickle
 from zipfile import ZipFile
-import sqlite3
-from datetime import date
+import datetime 
 import openpyxl as xl
 import yagmail
 from cryptography.fernet import Fernet
 import traceback
 import logging
+import bp_sql as bp
+import zipfile
+import time
 
+script_start_time = time.time()
 
-today = date.today().strftime("%m/%d/%y")
+today = datetime.date.today().strftime("%Y-%m-%d")
 
 #DB path
-lmp_db = os.path.expanduser("~") + '//Databases/LMP_DB.db'
+spp_db = 'SPP.db'
 
 #zip folder path
-zip_path = os.path.join(os.getcwd(),  'LMP_Zips')
+zip_path = os.path.join(os.getcwd(),  'SPP_Zips')
 
 #contents of zip folder path
-zip_list = os.listdir(os.path.join(os.getcwd(),  'LMP_Zips'))
+zip_list = os.listdir(os.path.join(os.getcwd(),  'SPP_Zips'))
 
 #used for saving files
-save_folder = 'LMP_Zips/'
+save_folder = 'SPP_Zips/'
 
 #Website and file type to download
 Domain = 'http://mis.ercot.com'
@@ -57,44 +60,8 @@ def send_email(email_subject, email_contents):
     
     yag.send(to=gmail_user, subject=email_subject, contents=email_contents)
 
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    conn = sqlite3.connect(db_file)
-    return conn
-
-
-def create_table(db_file, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:"""
-    try:
-        conn = create_connection(db_file)
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except (TypeError, NameError) as e:
-        print(e)
-
-    
-def delete_rows(db_file, delete_sql):
-    ''' Delete rows from a sqlite db
-    :param conn: Connection object
-    :param delete_sql: a delete statement'''
-    conn = create_connection(db_file)
-    c = conn.cursor()
-    c.execute(delete_sql)
-    conn.commit()
-    conn.close()
-    
-    
 def check_max_date(db_file):
-    conn = create_connection(db_file)
+    conn = bp.create_connection(db_file)
     max_date_df = pd.read_sql_query('''Select max(DELIVERY_DATE) as MAX_DELIVERY_DATE,  max(DELIVERY_HOUR) as MAX_DELIVERY_HOUR 
                                        from ercot_hist_spp 
                                        where delivery_date = (select max(delivery_date) from ercot_hist_spp where settlement_point_price is not null)''', conn)
@@ -104,13 +71,44 @@ def check_max_date(db_file):
     conn.close()
     return max_date, max_hour
 
+def get_sheet_list(file:str, year:int):
+    with zipfile.ZipFile(file) as zipped_file:
+        summary = zipped_file.open(r'xl/workbook.xml').read()
+    soup = BeautifulSoup(summary, 'xml')
+    sheet_list = [sheet.get("name") for sheet in soup.find_all("sheet")]
+
+    if year == min_file_year:
+        sheets_list = [x for x in sheet_list if revs_mon_dict.get(str(x[0:3]).upper())>= min_file_mon]
+
+    return sheet_list 
+    
+
+
+sql_create_spp_table = ''' Create Table if not exists ercot_hist_spp (
+                                    DELIVERY_DATE text,
+                                    DELIVERY_HOUR integer,
+                                    DELIVERY_INTERVAL integer,
+                                    REPEATED_HOUR_FLAG text,
+                                    SETTLEMENT_POINT_NAME text,
+                                    SETTLEMENT_POINT_TYPE text,
+                                    SETTLEMENT_POINT_PRICE real);
+                                '''
+
+bp.create_table(spp_db, sql_create_spp_table)
+
 
 try:
     #get max date of data in db
-    max_date, max_hour = check_max_date(lmp_db)
-    max_year = max_date.year
-    max_mon = max_date.month
-    max_day = max_date.day
+    max_date, max_hour = check_max_date(spp_db)
+    
+    if None not in (max_date, max_hour):
+        max_year = max_date.year
+        max_mon = max_date.month
+        max_day = max_date.day
+    else:
+        max_year, max_mon, max_day = 2000, 1 , 1
+        max_date = date(max_year, max_mon, max_day).strftime("%m/%d/%y")
+        
 
     max_mon_abrv = mon_dict.get(max_mon)
 
@@ -167,7 +165,6 @@ try:
 
 
     #dl_zip_df Download each zip from df that hasn't been downloaded already and save to specific folder in wd
-
     for i in range(len(join_df)):
         #download new zip and delete any old zips
         if (join_df['ZipFolderDate'][i]>join_df['filedate'][i]) and (join_df['ZipFolderYear'][i]>= min_file_year):
@@ -176,33 +173,20 @@ try:
                 file.write(response.content)
 
         elif 0 < join_df['filedate'][i] < new_max_file_date:
-            os.remove(os.path.join(os.getcwd(),  'LMP_Zips', join_df['filename'][i]))
+            os.remove(os.path.join(os.getcwd(),  'SPP_Zips', join_df['filename'][i]))
 
 
     #extracts the xlsx from each zip and places in same directory
-    for file in os.listdir(zip_path):
-        if '.zip' in file and min_file_year:
+    for file in [j for j in os.listdir(zip_path) if '.zip' in j]:
             with ZipFile(zip_path + '/' + file, 'r') as zipObj:
                 zipObj.extractall(path=zip_path)
 
-    sql_create_lmp_table = ''' Create Table if not exists ercot_hist_spp (
-                                        DELIVERY_DATE text,
-                                        DELIVERY_HOUR integer,
-                                        DELIVERY_INTERVAL integer,
-                                        REPEATED_HOUR_FLAG text,
-                                        SETTLEMENT_POINT_NAME text,
-                                        SETTLEMENT_POINT_TYPE text,
-                                        SETTLEMENT_POINT_PRICE real);
-                                    '''
 
-    create_table(lmp_db, sql_create_lmp_table)
 
-    delete_rows(lmp_db, delete_sql='''delete from ercot_hist_spp where settlement_point_price is null''')
+    bp.delete_rows(spp_db, delete_sql='''delete from ercot_hist_spp where settlement_point_price is null''')
 
-    file_list = []
-    month_list = []
-    start_date_list = []
-    end_date_list = []
+    #create blank lists
+    file_list, month_list, start_date_list, end_date_list, time_list  = ([] for i in range(5))
 
     column_dict = {'Delivery Date':'DELIVERY_DATE'
                    , 'Delivery Hour':'DELIVERY_HOUR'
@@ -212,41 +196,48 @@ try:
                    ,'Settlement Point Type':'SETTLEMENT_POINT_TYPE'
                    ,'Settlement Point Price':'SETTLEMENT_POINT_PRICE'}
 
-    conn = conn = create_connection(lmp_db)
 
-    for file in os.listdir(os.path.join(os.getcwd(),  'LMP_Zips')):
-        if '.xlsx' in file and int(file[-9:-5]) >= min_file_year:
-            file_path = os.path.join(os.getcwd(),  'LMP_Zips', file)
-            wb = xl.load_workbook(file_path, read_only=True)
-            for sheet in wb.sheetnames:
-                if revs_mon_dict.get(str(sheet[0:3]).upper())>= min_file_mon:
-                    data = wb[sheet].values
-                    columns = next(data)[0:]
-                    upload_sheet = pd.DataFrame(data, columns=columns)
-                    upload_sheet.dropna(inplace=True)
-                    upload_sheet['Delivery Date'] = pd.to_datetime(upload_sheet['Delivery Date'])
-                    upload_sheet = upload_sheet[upload_sheet['Delivery Date']>max_date]
-                    if len(upload_sheet)>0:
-                        min_del_date = min(upload_sheet['Delivery Date'])
-                        max_del_date = max(upload_sheet['Delivery Date'])
-                        upload_sheet.rename(columns=column_dict, inplace=True)
-                        upload_sheet.to_sql(name='ercot_hist_spp', con=conn, if_exists='append', index=False)
 
-                        file_list.append(file)
-                        month_list.append(sheet)
-                        start_date_list.append(min_del_date)
-                        end_date_list.append(max_del_date)
+    conn = bp.create_connection(spp_db)
 
-    conn.execute("VACUUM")
+    get_new_data_start_time = time.time()
+     
+    for file in [f for f  in sorted(os.listdir(os.path.join(os.getcwd(),  'SPP_Zips'))) if '.xlsx' in f]:
+
+        file_year = int(file[-9:-5])
+
+        if '.xlsx' in file and file_year >= min_file_year:
+            file_path = os.path.join(os.getcwd(),  'SPP_Zips', file)
+            sheet_list = get_sheet_list(file_path, file_year)
+            upload_df = pd.concat(pd.read_excel(file_path, sheet_name=sheet_list), ignore_index=True)
+            upload_df.dropna(inplace=True)
+            upload_df['Delivery Date'] = pd.to_datetime(upload_df['Delivery Date'])
+            upload_df = upload_df[upload_df['Delivery Date']>max_date]
+
+            if len(upload_df)>0:
+                min_del_date = min(upload_df['Delivery Date'])
+                max_del_date = max(upload_df['Delivery Date'])
+                upload_df.rename(columns=column_dict, inplace=True)
+                upload_df.to_sql(name='ercot_hist_spp', con=conn, if_exists='append', index=False)
+
+                file_list.append(file)
+                start_date_list.append(min_del_date)
+                end_date_list.append(max_del_date)
+                time_list.append(time.time() - get_new_data_start_time)
+
+    bp.vacuum_db(spp_db)
     conn.close()
     
-    email_dict = {'file':file_list, 'month': month_list, 'start_date': start_date_list, 'end_date':end_date_list}
+    email_dict = {'file':file_list, 'start_date': start_date_list, 'end_date':end_date_list, 'loop_duration': time_list}
     email_df = pd.DataFrame(email_dict)
     
-    
+
     
 except Exception as e:
     send_email(email_subject = 'ERCOT SPP Scrape - ' + today + ' - FAILED', email_contents='An error occured during the Eroct SPP Scraping Process: /n' + logging.error(traceback.format_exc()))
     
 else:
-     send_email(email_subject = 'ERCOT SPP Scrape - ' + today, email_contents=email_df)
+    total_script_time = time.time() - script_start_time
+    total_script_time = str(datetime.timedelta(seconds=total_script_time))
+    print(total_script_time)
+    send_email(email_subject = 'ERCOT SPP Scrape - ' + today, email_contents=['Script Time (Secs): '+ str(total_script_time) + '\n\n', email_df])
