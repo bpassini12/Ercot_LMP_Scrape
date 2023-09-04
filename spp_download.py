8e47af0255fed6424b885ce0c2b3aa5e117799c5
Brett Passini <Brett.Passini@Gmail.com>
2023-09-04 15:36:34 -0500

    fix for running on new linux sys

--- spp_download.py
+++ spp_download.py
@@ -18,8 +18,8 @@ import time
 #for chrome driver
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-# options = Options()
-# options.headless = True
+options = Options()
+options.add_argument('--headless')
 from pyvirtualdisplay import Display
 # display = Display(visible=0, size=(800, 800))  
 # display.start()
